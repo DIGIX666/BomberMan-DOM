b9649dd191aacb930813ead99b54edcbe2cfd10b
Santos <santoslukombo@gmail.com>
2023-08-12 20:36:39 +0200

    remove /asset/room.js

--- asset/scripts/room.js
+++ asset/scripts/room.js
@@ -1,3 +1,35 @@
+import { socket } from "./connect.js";
+
+let roomEnd = false
+let playersIn = []
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    setTimeout((timeWait, ) => {
+    
+        socket.onmessage = function (event) {
+            let dataPlayerEnter = JSON.parse(event.Data)
+
+            if (dataPlayerEnter!="") {
+                
+                playersIn.push(dataPlayerEnter)
+            }
+            console.log(dataPlayerEnter)
+        }
+    }, timeWait);
+})
+
+
+
+
+
+
+
+
+
+
 // Sélectionnez la div avec la classe "room"
 const roomDiv = document.querySelector('.room');
 
