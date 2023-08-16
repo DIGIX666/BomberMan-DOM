8d0cf2ee5c9d3a1189ea4b3871912f14377d5aac
théo dub <theodub@MacThox.local>
2023-08-16 16:21:46 +0200

    button pour aller dans la room

--- asset/client/scripts/setting-page.js
+++ asset/client/scripts/setting-page.js
@@ -10,4 +10,19 @@ function masquerElementsParClasse(nomClasse) {
 masquerElementsParClasse('room');
 masquerElementsParClasse('bomberman');
 
+// Sélectionnez le bouton par son ID
+const button = document.getElementById('button');
 
+// Sélectionnez la div avec la classe "room"
+const roomDiv = document.querySelector('.room');
+
+// Sélectionnez la section avec la classe "log"
+const logSection = document.querySelector('.log');
+
+// Ajoutez un gestionnaire d'événement pour le clic sur le bouton
+button.addEventListener('click', () => {
+    // Affichez la div de la salle
+    roomDiv.style.display = 'block';
+    // Masquez la section de journal
+    logSection.style.display = 'none';
+});
