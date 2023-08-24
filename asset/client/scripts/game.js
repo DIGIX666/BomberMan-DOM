d6a4c3eb510bafa262f8a095afb91597d21486b6
théo dub <theodub@MacThox.local>
2023-08-24 18:14:15 +0200

    deplacement bombe explosion de brick -> ok

--- asset/client/scripts/game.js
+++ asset/client/scripts/game.js
@@ -14,37 +14,12 @@ const mapData = [
     ['#', ' ', 'b', ' ', ' ', ' ', ' ', 'b', ' ', '#'],
     ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
 ];
-// document.addEventListener('DOMContentLoaded', () => {
-//     const bomberMan = document.querySelector('.game');
-//
-//     function createMap() {
-//         for (let row = 0; row < mapData.length; row++) {
-//             for (let col = 0; col < mapData[row].length; col++) {
-//                 const cell = document.createElement('div');
-//                 cell.classList.add('cell');
-//
-//                 if (mapData[row][col] === '#') {
-//                     cell.classList.add('wall');
-//                 } else if (mapData[row][col] === 'b') {
-//                     cell.classList.add('brick');
-//                 }
-//                 bomberMan.appendChild(cell);
-//             }
-//         }
-//         // Créer le personnage
-//         const character = document.createElement('div');
-//         character.classList.add('character'); // Ajoutez des styles pour positionner le personnage
-//         bomberMan.appendChild(character); // Ajoutez le personnage à l'élément parent
-//     }
-//
-//     createMap();
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
     const bomberMan = document.querySelector('.game');
     const characterWidth = 10; // Largeur du personnage
     const characterHeight = 67; // Hauteur du personnage
-
+///////////////////////////////////////////////////////////////////////////////////
     function createMap() {
         for (let row = 0; row < mapData.length; row++) {
             for (let col = 0; col < mapData[row].length; col++) {
@@ -54,6 +29,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (mapData[row][col] === '#') {
                     cell.classList.add('wall');
                 } else if (mapData[row][col] === 'b') {
+                    cell.setAttribute('data-row', row); // Ajouter l'attribut de données pour la ligne
+                    cell.setAttribute('data-col', col); // Ajouter l'attribut de données pour la colonne
                     cell.classList.add('brick');
                 }
                 bomberMan.appendChild(cell);
@@ -85,9 +62,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Vérifier les collisions avec les murs et les briques
             const newRow = Math.floor(newTop / 100);
-            const newCol = Math.floor(newLeft / 96);
+            const newCol = Math.floor(newLeft / 98);
             const bottomRow = Math.floor((newTop + characterHeight) / 100);
-            const rightCol = Math.floor((newLeft + characterWidth) / 96);
+            const rightCol = Math.floor((newLeft + characterWidth) / 98);
+
+            // Vérifier si le mouvement est possible
+            let canMove = true;
 
             if (
                 newRow >= 0 && newCol >= 0 && bottomRow < mapData.length && rightCol < mapData[0].length &&
@@ -102,67 +82,70 @@ document.addEventListener('DOMContentLoaded', () => {
             ) {
                 character.style.left = newLeft + 'px';
                 character.style.top = newTop + 'px';
+              }
+
+
+            // Ajouter la logique pour déposer une bombe avec la touche Espace
+            if (event.key === ' ') { // Touche Espace
+                    dropBomb(characterLeft + characterWidth / 2, characterTop + characterHeight / 2);
             }
         });
     }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function dropBomb(x, y) {
+        const bomb = document.createElement('div');
+        bomb.classList.add('bombe');
+        bomb.style.left = x + 'px';
+        bomb.style.top = y + 'px';
+        bomberMan.appendChild(bomb);
+
+        // Programmer l'animation d'explosion après 3 secondes
+        setTimeout(function() {
+            bomberMan.removeChild(bomb); // Supprimer l'élément de la bombe
+
+            // Créer l'élément d'explosion
+            const explosion = document.createElement('div');
+            explosion.classList.add('explosion');
+            explosion.style.left = x + 'px';
+            explosion.style.top = y + 'px';
+            bomberMan.appendChild(explosion);
+
+            // Vérifier les collisions avec les briques
+            const bricks = document.querySelectorAll('.brick');
+            bricks.forEach((brick) => {
+                if (checkCollision(explosion, brick)) {
+                    const brickRow = parseInt(brick.getAttribute('data-row'));
+                    const brickCol = parseInt(brick.getAttribute('data-col'));
+                    brick.style.visibility = 'hidden'; // Cacher la brique visuellement
+                    brick.classList.remove('brick'); // Retirer la classe "brick"
+
+                    mapData[brickRow][brickCol] = ' '; // Mettre à jour le modèle de données
+                }
+            });
+
+            // Supprimer l'élément d'explosion après un délai
+            setTimeout(function() {
+                bomberMan.removeChild(explosion);
+            }, 1000); // Supprimer l'explosion après 1 seconde
+        }, 2000); // 3 secondes
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function checkCollision(element1, element2) {
+        const rect1 = element1.getBoundingClientRect();
+        const rect2 = element2.getBoundingClientRect();
+        return (
+            rect1.left < rect2.right &&
+            rect1.right > rect2.left &&
+            rect1.top < rect2.bottom &&
+            rect1.bottom > rect2.top
+        );
+    }
+
 
     createMap();
 });
+//////////////////////////////// FIN JEU ////////////////////////////////
 
-//////////////////////////////////////////////////////////////
 
-
-////////////////// DIRECTION DU PERSONNAGE /////////////////////
-// document.addEventListener('keydown', (event) => {
-//     const character = document.querySelector('.character');
-//     const characterStyle = getComputedStyle(character);
-//     const characterLeft = parseInt(characterStyle.left);
-//     const characterTop = parseInt(characterStyle.top);
-//     const characterWidth = parseInt(characterStyle.width);
-//     const characterHeight = parseInt(characterStyle.height); // Nouvelle ligne
-//
-//     const newPosition = { left: characterLeft, top: characterTop };
-//
-//     if (event.key === 'ArrowRight') {
-//         newPosition.left += 10; // Ajustez la valeur de déplacement
-//     }
-//     if (event.key === 'ArrowLeft') {
-//         newPosition.left -= 10;
-//     }
-//     if (event.key === 'ArrowUp') {
-//         newPosition.top -= 10;
-//     }
-//     if (event.key === 'ArrowDown') {
-//         newPosition.top += 10;
-//     }
-//
-//     // Vérifier si la nouvelle position contient un mur ou un obstacle
-//     const newRow = Math.floor(newPosition.top / 97); // Taille d'une cellule
-//     const newCol = Math.floor(newPosition.left / 96);  // Taille d'une cellule
-//
-// // Vérifier toutes les coins du personnage pour collision
-//     const characterBottomRow = newRow + Math.floor(characterHeight / 97);
-//     const characterRightCol = newCol + Math.floor(characterWidth / 96);
-//
-//
-//     const bottomRow = Math.floor((newPosition.top + characterHeight) / 97); // Nouvelle ligne
-//     const rightCol = Math.floor((newPosition.left + characterWidth) / 96); // Nouvelle colonne
-//
-//
-//     if (
-//         mapData[newRow][newCol] !== '#' &&
-//         mapData[newRow][characterRightCol] !== '#' &&
-//         mapData[characterBottomRow][newCol] !== '#' &&
-//         mapData[characterBottomRow][characterRightCol] !== '#' &&
-//         mapData[newRow][newCol] !== 'b' &&
-//         mapData[newRow][characterRightCol] !== 'b' &&
-//         mapData[characterBottomRow][newCol] !== 'b' &&
-//         mapData[characterBottomRow][characterRightCol] !== 'b'
-//     ) {
-//         // Mettre à jour la position seulement si la nouvelle position est libre
-//         character.style.left = newPosition.left + 'px';
-//         character.style.top = newPosition.top + 'px';
-//     }
-//
-// });
-//////////////////////////////////////////////////////////////////////////////////////////
