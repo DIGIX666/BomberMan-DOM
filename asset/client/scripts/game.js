5a0e4d59a0d4a5dbc1b2f5a7d69285376cd0a485
théo dub <theodub@MacThox.local>
2023-09-12 16:54:41 +0200

    merge test branch

--- asset/client/scripts/game.js
+++ asset/client/scripts/game.js
@@ -1,6 +1,19 @@
-// import {createElement} from "../../framework/domUtils";
-// import {appendChild} from "../../framework/domUtils";
-// import {getElementById} from "../../framework/domUtils";
+import { socket } from "../connect";
+
+
+////////////////Player//////////////////
+class Player {
+    constructor(namePlayer,adress,direction,lives,bombe){
+
+        this.namePlayer = namePlayer = ""
+        this.adress = adress = ""
+        this.direction = direction = ""
+        this.lives = lives = 3
+        this.bombe = bombe = false
+    }
+}
+///////////////////////////////////////
+
 
 
 //////////////////////// CREATE MAP //////////////
@@ -14,13 +27,61 @@ const mapData = [
     ['#', ' ', 'b', ' ', ' ', ' ', ' ', 'b', ' ', '#'],
     ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
 ];
+//////////////////////////////////////////////////////////////////////////////////
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const bomberMan = document.querySelector('.game');
+    const character = document.createElement('div');
     const characterWidth = 10; // Largeur du personnage
     const characterHeight = 67; // Hauteur du personnage
     let hitPlayer = false;
-///////////////////////////////////////////////////////////////////////////////////
+    let P1 = document.querySelector(".Player1")
+    let P2 = document.querySelector(".Player2")
+    let P3 = document.querySelector(".Player3")
+    let P4 = document.querySelector(".Player4")
+
+    const characterStyle = getComputedStyle(character);
+    const characterLeft = parseInt(characterStyle.left);
+    const characterTop = parseInt(characterStyle.top);
+
+    const player = new Player
+    let Direction = player.direction
+    let currentLife = player.lives
+    let playerName = player.namePlayer
+    let adress = player.namePlayer
+    let newLeft = 0
+    let newTop = 0
+
+
+    // Créer le personnage
+    character.classList.add('character');
+    bomberMan.appendChild(character);
+
+    ///////////////////Recevoir les joueurs///////////////////////////////////////////
+    socket.onmessage = function (event) {
+        let Server = JSON.parse(event.data)
+        if (Server.type == "Players") {
+            let Players = []
+            console.log("Server In Game:", Server)
+            Server.data.allPlayers.forEach(element => {
+                Players.push(element)
+            });
+
+            console.log("Players tab:", Players)
+            P1.innerHTML = Players[0]
+            P2.innerHTML = Players[1]
+            P3.innerHTML = Players[2]
+            P4.innerHTML = Players[3]
+            playerName = Server.data.name
+            adress = Server.data.clientAdress
+
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////
+
     function createMap() {
         for (let row = 0; row < mapData.length; row++) {
             for (let col = 0; col < mapData[row].length; col++) {
@@ -37,29 +98,96 @@ document.addEventListener('DOMContentLoaded', () => {
                 bomberMan.appendChild(cell);
             }
         }
-        // Créer le personnage
-        const character = document.createElement('div');
-        character.classList.add('character');
-        bomberMan.appendChild(character);
 
+
+    }
+    function Moving(character, socket) {
+        
+        socket.onmessage = (event) => {
+            let ServerMovement = JSON.parse(event.data)
+            
+            if (ServerMovement.Type === "playerMoved") {
+                
+                
+                if (ServerMovement.data.name === playerName) {
+                    if (ServerMovement.data.direction === "Right" || ServerMovement.data.direction === "Left") {
+                        Direction = ServerMovement.data.direction
+                        newLeft = characterLeft
+                        newLeft += ServerMovement.data.direction 
+                    }
+
+                    if (ServerMovement.data.direction === "Up" || ServerMovement.data.direction === "Down") {
+                        Direction = ServerMovement.data.direction
+                        newTop = characterTop
+                        newTop += ServerMovement.data.direction 
+                    }
+                }
+            }  
+        }
+        
         // Gérer le mouvement du personnage avec les flèches du clavier
         document.addEventListener('keydown', (event) => {
-            const characterStyle = getComputedStyle(character);
-            const characterLeft = parseInt(characterStyle.left);
-            const characterTop = parseInt(characterStyle.top);
+            // const characterStyle = getComputedStyle(character);
+            // const characterLeft = parseInt(characterStyle.left);
+            // const characterTop = parseInt(characterStyle.top);
 
-            let newLeft = characterLeft;
-            let newTop = characterTop;
+            newLeft = characterLeft;
+            newTop = characterTop;
             hitPlayer = false;
 
             if (event.key === 'ArrowRight') {
                 newLeft += 10;
+                Direction = "Right"
+                socket.send(JSON.stringify({
+                    Type: "PlayerMoving",
+                    data: {
+                        direction: Direction,
+                        player: adress,
+                        name: playerName,
+
+
+                    }
+                }))
             } else if (event.key === 'ArrowLeft') {
                 newLeft -= 10;
+                Direction = "Left"
+
+                socket.send(JSON.stringify({
+                    Type: "PlayerMoving",
+                    data: {
+                        direction: Direction,
+                        player: adress,
+                        name: playerName
+
+                    }
+                }))
             } else if (event.key === 'ArrowUp') {
                 newTop -= 10;
+                Direction  = "Up"
+
+                socket.send(JSON.stringify({
+                    Type: "PlayerMoving",
+                    data: {
+                        direction: Direction,
+                        player: adress,
+                        name: playerName
+
+
+                    }
+                }))
             } else if (event.key === 'ArrowDown') {
                 newTop += 10;
+                Direction = "Down"
+
+                socket.send(JSON.stringify({
+                    Type: "PlayerMoving",
+                    data: {
+                        direction: Direction,
+                        player: adress,
+                        name: playerName
+
+                    }
+                }))
             }
 
             // Vérifier les collisions avec les murs et les briques
@@ -84,18 +212,29 @@ document.addEventListener('DOMContentLoaded', () => {
             ) {
                 character.style.left = newLeft + 'px';
                 character.style.top = newTop + 'px';
-              }
+            }
 
 
             // Ajouter la logique pour déposer une bombe avec la touche Espace
             if (event.key === ' ') { // Touche Espace
-                    dropBomb(character,characterLeft + characterWidth / 2, characterTop + characterHeight / 2);
+                dropBomb(character, characterLeft + characterWidth / 2, characterTop + characterHeight / 2,currentLife);
+                socket.send(JSON.stringify({
+                    Type: "Player Dropped Bomb",
+                    data:{
+                        name: playerName,
+                        adress: playerAdress,
+                        x:characterLeft + characterWidth / 2,
+                        y:characterTop + characterHeight / 2,
+                        currentLife: currentLife
+
+                    }
+                }))
             }
         });
     }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    function dropBomb(character,x, y) {
+    function dropBomb(character, x, y,currentLife) {
         const bomb = document.createElement('div');
         bomb.classList.add('bombe');
         bomb.style.left = x + 'px';
@@ -103,7 +242,7 @@ document.addEventListener('DOMContentLoaded', () => {
         bomberMan.appendChild(bomb);
 
         // Programmer l'animation d'explosion après 3 secondes
-        setTimeout(function() {
+        setTimeout(function () {
             bomberMan.removeChild(bomb); // Supprimer l'élément de la bombe
 
             // Créer l'élément d'explosion
@@ -128,22 +267,22 @@ document.addEventListener('DOMContentLoaded', () => {
                         !hitPlayer
                     ) {
                         hitPlayer = true; // Marquer que le joueur a été touché
-                        reduceLife(); // Appeler la fonction pour réduire la vie du joueur
+                        reduceLife(currentLife); // Appeler la fonction pour réduire la vie du joueur
                         console.log("vie perdu");
                     }
                 }
             });
 
             // Supprimer l'élément d'explosion après un délai
-            setTimeout(function() {
+            setTimeout(function () {
                 bomberMan.removeChild(explosion);
             }, 1000); // Supprimer l'explosion après 1 seconde
         }, 2000); // 3 secondes
     }
 
-    function reduceLife() {
+    function reduceLife(currentLife) {
         const lifeElement = document.querySelector('.life');
-        let currentLife = parseInt(lifeElement.textContent);
+        currentLife = parseInt(lifeElement.textContent);
 
         if (currentLife > 0) {
             currentLife--;
@@ -153,6 +292,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 const characterLife = document.querySelector('.character');
                 bomberMan.removeChild(characterLife);
                 console.log("Game over!");
+                socket.send(JSON.stringify({
+                    Type: "GAME OVER",
+                    Data: {
+                        name: namePlayer,
+                        adress: playerAdress,
+                    }
+                }))
             }
         }
     }
@@ -171,7 +317,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     createMap();
+    Moving(character,socket);
+
+
 });
 //////////////////////////////// FIN JEU ////////////////////////////////
-
-
